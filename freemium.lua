5bc5fc02847f2ada7b24f225c538b0ba8896dde0
CupPink <mardenzy.swk@gmail.com>
2025-02-02 21:14:46 +0700

    Update freemium.lua

--- freemium.lua
+++ freemium.lua
@@ -1,8 +1,7 @@
 local Game_ID = game.GameId
 local Games = {
-    --[5750914919] = "https://api.luarmor.net/files/v3/loaders/ad5cfdc73c1570fe1f4fdde962dcd5dc.lua", -- Fisch
-    [5750914919] = "https://api.luarmor.net/files/v3/loaders/2b78b58f6d8e9a8ada7ea89f86131f8f.lua", -- Fisch
-    [6705549208] = "https://api.luarmor.net/files/v3/loaders/2b78b58f6d8e9a8ada7ea89f86131f8f.lua", -- Dig it
-    [2936053166] = "https://api.luarmor.net/files/v3/loaders/314d369df5aa3934fd3588659bfada04.lua", -- Squid Game X
+    [5750914919] = "https://raw.githubusercontent.com/CupPink/scripts/main/Games/Fisch.lua", -- Fisch
+    [6705549208] = "https://raw.githubusercontent.com/CupPink/scripts/main/Games/Digit.lua", -- Dig it
+    [6489669774] = "https://raw.githubusercontent.com/CupPink/scripts/main/Games/Exiled.lua", -- Exiled
 }
 loadstring(game:HttpGet(Games[Game_ID]))()
