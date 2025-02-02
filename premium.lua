4a1dfa2196376f0863e087a17b0110ef4bc2b23c
CupPink <mardenzy.swk@gmail.com>
2025-02-02 21:31:24 +0700

    Update premium.lua

--- premium.lua
+++ premium.lua
@@ -1,5 +1,5 @@
 local Game_ID = game.GameId
 local Games = {
-    [5750914919] = "https://api.luarmor.net/files/v3/loaders/fceb40868fe3ffeda4bcc237a8920634.lua", -- Fisch
+    --
 }
 loadstring(game:HttpGet(Games[Game_ID]))()
