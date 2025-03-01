05d20c051141829688d2ae5cb24abb0ba669e3ec
CupPink <mardenzy.swk@gmail.com>
2025-03-01 22:33:57 +0700

    Delete freemium.lua

--- freemium.lua
+++
@@ -1,7 +0,0 @@
-local Game_ID = game.GameId
-local Games = {
-    [5750914919] = "https://raw.githubusercontent.com/CupPink/scripts/main/Games/Fisch.lua", -- Fisch
-    [6705549208] = "https://raw.githubusercontent.com/CupPink/scripts/main/Games/Digit.lua", -- Dig it
-    [6489669774] = "https://raw.githubusercontent.com/CupPink/scripts/main/Games/Exiled.lua", -- Exiled
-}
-loadstring(game:HttpGet(Games[Game_ID]))()
